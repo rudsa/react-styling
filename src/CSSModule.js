3779e9e8a2d463e249901952f83011e508e61721
rudsa <rudsa@naver.com>
2023-05-14 20:16:39 +0900

    react-styleComponent

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,8 +1,11 @@
-import styles from "./CSSModule.module.css";
+import classNames from "classnames/bind";
+import styles from "./CSSModule.module.scss";
+
+const cx = classNames.bind(styles);
 
 const CSSModule = () => {
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx("wrapper", "inverted")}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
